1497b4149a4416955e2aaaa073e5078303ea158c
Daniel Choi <dhchoi@gmail.com>
2012-10-04 18:03:49 -0400

    Example of decoding serialized JSON

--- Coord.hs
+++ Coord.hs
@@ -4,17 +4,21 @@
 module Main where
 
 import Data.Aeson
-import GHC.Generics
+import GHC.Generics (Generic)
 import Data.ByteString.Lazy.Char8 as Char8
 
-data Coord = Coord { x :: Double, y :: Double } deriving Generic
+data Coord = Coord { x :: Double, y :: Double } deriving (Generic, Show)
 
 instance ToJSON Coord
+instance FromJSON Coord
+
+example = Coord { x = 20.0, y = 99.9 }
 
 main = do
   Prelude.putStrLn "hello"
-  let c = Coord { x = 20.0, y = 99.9 }
-  Char8.putStrLn $ encode c  
+  Char8.putStrLn $ encode example
   -- outputs 
   -- {"y":99.9,"x":20.0}
+  Prelude.print $ ((decode $ encode example ) :: Maybe Coord)
+
 
---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+
+Parse nested JSON
+
+  http://stackoverflow.com/questions/6826678/how-to-parse-nested-json-with-aeson
+
+
+For simple Aeson parsing example with a data type
+
+  https://gist.github.com/985645
